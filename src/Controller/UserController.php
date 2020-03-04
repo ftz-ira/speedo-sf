ae75f3e0e1de9467565db569186c37bea5e0d25a
ftz <frantziraci@gmail.com>
2020-03-04 11:16:33 +0100

    form-user a completer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,31 +27,39 @@ class UserController extends AbstractController
     /**
      * @Route("/user/add", name="add-user")
      */
-    public function addUser(){
-        $em = $this->getDoctrine()->getManager();
-        $date = new \DateTime();
-
-        $role = new Role();
-        $role->setActive(1);
-        $role->setName('admin');
-        $em->persist($role);
+    public function new(){
 
         $user = new User();
-        $user->setActive(1);
-        $user->setBirthday($date);
-        $user->setCreatedDate($date);
-        $user->setEmail('ftz@aim.com');
-        $user->setPassword("azerty");
-        $user->setPseudo('ftz-1');
-        $user->setRole($role);
-        $user->setStatus(null);
-
-        $em->persist($user);
-        $em->flush();
-
-        return $this->render('user/index.html.twig', [
-            'user_name' => $user->getPseudo(),
+        $form = $this->createForm(UserType::class, $user);
+        return $this->render('user/index.html.twig',[
+            'form' => $form->createView()
         ]);
+    }
 
+    public function demo(){
+  //      $em = $this->getDoctrine()->getManager();
+//        $date = new \DateTime();
+//
+//        $role = new Role();
+//        $role->setActive(1);
+//        $role->setName('admin');
+//        $em->persist($role);
+//
+//        $user = new User();
+//        $user->setActive(1);
+//        $user->setBirthday($date);
+//        $user->setCreatedDate($date);
+//        $user->setEmail('ftz@aim.com');
+//        $user->setPassword("azerty");
+//        $user->setPseudo('ftz-1');
+//        $user->setRole($role);
+//        $user->setStatus(null);
+//
+//        $em->persist($user);
+//        $em->flush();
+//
+//        return $this->render('user/index.html.twig', [
+//            'user_name' => $user->getPseudo(),
+//        ]);
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +14,13 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
+            ->add('pseudo',TextType::class)
             ->add('birthday')
             ->add('createdDate')
             ->add('email')
             ->add('password')
             ->add('active')
-            ->add('sponsorshipLink')
-            ->add('role')
-            ->add('status')
+            ->add('save', SubmitType::class)
         ;
     }
 
